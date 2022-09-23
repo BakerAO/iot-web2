b28b5596019e18afba4de4423913780598868314
BakerAO <dpshunter@gmail.com>
2022-09-22 20:08:17 -0500

    Update

--- src/data/api.js
+++ src/data/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 
 let baseURL = 'https://api.tidoba.com'
+// let baseURL = 'http://10.0.0.196:8081'
 // if (window.location.hostname === 'localhost') {
 //   baseURL = 'http://localhost:8081'
 // }
