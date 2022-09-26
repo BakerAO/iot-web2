ebf92e0ff5381860d3be85b00e1636196d154d72
BakerAO <dpshunter@gmail.com>
2022-09-26 15:01:35 -0500

    Update

--- src/components/Body/Schedule/index.js
+++ src/components/Body/Schedule/index.js
@@ -1,37 +1,58 @@
-import { useEffect, useState } from 'react'
-import { Grid } from "@mui/material"
+import { useCallback, useEffect, useState } from 'react'
+import { Button, Grid } from "@mui/material"
 import api from  '../../../data/api'
 import { useDataContext } from '../../../context/DataContext'
 
 export default function Schedule() {
   const { data } = useDataContext()
-  const [deviceData, setDeviceData] = useState(null)
+  const [deviceData, setDeviceData] = useState([])
+
+  const getDeviceData = useCallback(async () => {
+    try {
+      const token = localStorage.getItem('innov8_token')
+      if (token && data?.selectedDeviceId) {
+        const headers = { auth_token: token }
+        const result = await api.get(`/schedule/${data.selectedDeviceId}`, { headers })
+        setDeviceData(result?.data?.schedules ?? [])
+      }
+    } catch (e) {
+      setDeviceData([])
+      console.error(e)
+    }
+  }, [data?.selectedDeviceId])
 
   useEffect(() => {
-    const getDeviceData = async () => {
-      try {
-        const token = localStorage.getItem('innov8_token')
-        if (token && data?.selectedDeviceId) {
-          const headers = { auth_token: token }
-          const result = await api.get(`/schedule/${data.selectedDeviceId}`, { headers })
-          setDeviceData(result?.data)
-        }
-      } catch (e) {
-        setDeviceData(null)
-        console.error(e)
-      }
-    }
-
     getDeviceData()
-  }, [data?.selectedDeviceId])
+  }, [data?.selectedDeviceId, getDeviceData])
+
+  const handleClick = async () => {
+    try {
+      const token = localStorage.getItem('innov8_token')
+      if (token) {
+        const payload = {
+          frequency: 'daily',
+          startTime: '12:00',
+          durationMinutes: 15
+        }
+        const options = { headers: { auth_token: `${token}` } }
+        await api.post(`/schedule/${data.selectedDeviceId}`, payload, options)
+        await getDeviceData()
+      }
+    } catch (e) {
+      console.log(e)
+    }
+  }
 
   return (
     <Grid container spacing={1} className="iot-schedule">
       <Grid item xs={12} sx={{ height: '42vh' }}>
-        <div>{deviceData}</div>
+        <div>{JSON.stringify(deviceData)}</div>
       </Grid>
       <Grid item xs={12} sx={{ height: '40vh', overflow: 'auto' }}>
         <div>{data?.selectedDeviceId}</div>
+        <Button variant="outlined" onClick={handleClick}>
+          Click Me
+        </Button>
       </Grid>
     </Grid>
   )
