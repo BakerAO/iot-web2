4bb5794a68d84f7d7bd7cf35a65f89c29eb9cc20
BakerAO <dpshunter@gmail.com>
2023-02-05 16:45:50 -0600

    Update

--- src/data/api.js
+++ src/data/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
-let baseURL = 'https://api.tidoba.com'
-if (window.location.hostname === 'localhost') {
-  baseURL = 'http://10.0.0.196:8081'
-}
+let baseURL = 'http://api.bakerao.site'
+// if (window.location.hostname === 'localhost') {
+//   baseURL = 'http://10.0.0.196:8081'
+// }
 
 export default axios.create({ baseURL })
