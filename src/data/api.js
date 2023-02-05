91c26149af36db82113fbc9db1d8db935084cf13
BakerAO <dpshunter@gmail.com>
2023-02-05 17:21:36 -0600

    Update

--- src/data/api.js
+++ src/data/api.js
@@ -1,6 +1,7 @@
 import axios from 'axios'
 
-let baseURL = 'http://api.bakerao.site'
+// let baseURL = 'http://api.bakerao.site'
+let baseURL = 'http://10.0.0.196:8081'
 // if (window.location.hostname === 'localhost') {
 //   baseURL = 'http://10.0.0.196:8081'
 // }
