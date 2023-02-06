a099ea86cc30ecf220e09954a4a4c52a40d82149
BakerAO <dpshunter@gmail.com>
2023-02-05 19:52:54 -0600

    Update

--- src/data/api.js
+++ src/data/api.js
@@ -1,9 +1,6 @@
 import axios from 'axios'
 
-// let baseURL = 'http://api.bakerao.site'
-let baseURL = 'http://10.0.0.196:8081'
-// if (window.location.hostname === 'localhost') {
-//   baseURL = 'http://10.0.0.196:8081'
-// }
+let baseURL = 'http://api.bakerao.site'
+// let baseURL = 'http://localhost:8081'
 
 export default axios.create({ baseURL })
